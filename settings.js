e9e818d1d0ac53e37debda6f92aac5523eb5334b
newbot10 <115184504+newbot10@users.noreply.github.com>
2022-10-07 00:25:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['0768251691']
+global.premium = ['0768251691']
+global.ownernomer = '0768251691'
+global.ownername = '🐦Star Boy CS CHAMIYA'
+global.botname = 'CS_CHAMIYA'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'CS_CHAMIYA'
+global.author = '🐦Star Boy CS'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
